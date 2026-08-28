fb0361fb44ddf37cdf562a32539eea24960c85a4
Adarsh Kumar <adarshkumar4145@gmail.com>
2026-08-28 19:05:30 +0530

    added eureka server, added config server, config server has native ie local profile , and github profile. also created folders cofig-repo-github and config-repo-native (native=local) , tested local , will test github wit this push

--- cloudGateway/src/main/java/io/github/adarsh4145/cloudGateway/CloudGatewayApplication.java
+++ cloudGateway/src/main/java/io/github/adarsh4145/cloudGateway/CloudGatewayApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloudGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudGatewayApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(CloudGatewayApplication.class, args);
+  }
 }
